e56fd3c2406ebe2e0e9186e79e02b1c782ce11ca
pR916 <20me0114@vvce.ac.in>
2021-03-19 04:50:28 +0000

    Yes

--- Program-2.c
+++ Program-2.c
@@ -13,12 +13,13 @@ int main()
 {
 	      char str1[50], str2[50];
       	int i;
+      	printf("Enter a string-1: ");
 	      scanf("%s",str1);
 	      for (i=0;i<50;i++)                  //Complete the code in for-loop & copy logic
   {                          
 		            str2[i]=str1[i] ;
 	      }
-	      str2[i]=str1[50] ;                  //Complete the statement
+	      str2[i]=str1[50] ;                //Complete the statement
 	      printf("The copied String-2 is %s",str2);
 	      return 0;
 }
