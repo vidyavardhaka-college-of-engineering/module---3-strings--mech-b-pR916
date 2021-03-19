2cdc9c68622543783d0ddbf11304ae89ef958db6
pR916 <20me0114@vvce.ac.in>
2021-03-19 04:37:06 +0000

    S

--- Program-1.c
+++ Program-1.c
@@ -15,7 +15,7 @@ int main()
               char a[100];
 printf("Enter a String: ");
 scanf("%s",a);  
-printf("input string is %s",a);                   //read the string from user
+printf("input string is %s",a);                  //read the string                from user
                   //Print the string
 return 0;
 }
