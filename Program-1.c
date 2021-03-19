95fad53c417ab4110a904d0ca02e269de5ad0f35
pR916 <20me0114@vvce.ac.in>
2021-03-19 07:20:57 +0000

    Yes

--- Program-1.c
+++ Program-1.c
@@ -15,7 +15,7 @@ int main()
                       char a[100];
 printf("Enter a String: ");
 scanf("%s",a);  
-printf("input string is %s\n",a);                  
+printf("input string is \n %s",a);                  
                //read the string from user
                //print the string
 return 0; 
